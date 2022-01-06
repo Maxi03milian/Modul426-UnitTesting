da0a1deedc9ec9ba63e872fc05cdd730a1c52b0e
Maxi <46729527+Maxi03milian@users.noreply.github.com>
2022-01-06 16:40:59 +0100

    implemented addition test (10, 20)
    With 2 big numbers

---
+++ src/test/java/ch/bbw/pr/savecalculator/SaveCalculatorTest.java
@@ -0,0 +1,12 @@
+package ch.bbw.pr.savecalculator;
+
+import junit.framework.TestCase;
+public class SaveCalculatorTest extends TestCase {
+
+    public void testSumme() {
+        SaveCalculator testee = new SaveCalculator();
+        int value1 = 10;
+        int value2 = 20;
+        assertEquals(30, testee.summe(value1, value2));
+    }
+}
